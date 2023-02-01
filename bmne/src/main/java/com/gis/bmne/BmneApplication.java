c6fe5045cb31d0eabe5666751ff415fd58b2c0f4
Raden Rovčanin <radenrovcanin12@gmail.com>
2023-02-01 19:58:41 +0100

    Project initialization

---
+++ bmne/src/main/java/com/gis/bmne/BmneApplication.java
@@ -0,0 +1,13 @@
+package com.gis.bmne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmneApplication.class, args);
+	}
+
+}
---
+++ bmne/src/test/java/com/gis/bmne/BmneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.bmne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
