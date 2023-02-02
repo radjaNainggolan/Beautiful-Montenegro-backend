325d539899fd076cc5b9636a1e0f08859b299bd9
Raden Rovčanin <radenrovcanin12@gmail.com>
2023-02-02 14:19:55 +0100

    Forgot GetMapping annotation

--- bmne/src/main/java/com/gis/bmne/controllers/LocationController.java
+++ bmne/src/main/java/com/gis/bmne/controllers/LocationController.java
@@ -3,6 +3,7 @@ package com.gis.bmne.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class LocationController {
 		this.locationService = locationService;
 	}
 	
+	@GetMapping
 	public List<Location> getAllLocations(){
 		return locationService.getAllLocations();
 	}
